4555a46394dbde45b242e373bed994a157cfc839
bsm amr <bsmamr@gmail.com>
2026-03-02 17:11:54 +0100

    fix: use VITE_API_URL in axios config

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,13 +1,11 @@
 import axios from 'axios';
-axios.defaults.baseURL = "https://plateforme-e-learning-ndlr.vercel.app/api/"
-axios.defaults.baseURL= 'http://localhost:4000/api/'
-// Créer une instance Axios avec baseURL
-const axiosInstance = axios.create({
-  baseURL: axios.defaults.baseURL
 
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api/`
+    : 'http://localhost:4000/api/'
 });
 
-// Intercepteur pour ajouter le token
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('CC_Token');
@@ -16,9 +14,7 @@ axiosInstance.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default axiosInstance;
